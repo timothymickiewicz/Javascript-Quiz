8071775ad9fef35f8bde475b16dbcaefa4714cc3
Timothy Mickiewicz <timothy.mickiewicz@gmail.com>
2020-02-24 04:37:12 -0600

    further formatting on button click, created function for text addition on to be embedded on click

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var header = document.querySelector("h1");
 var btn = document.querySelector(".htmlBtn");
 var background = document.querySelector("body");
-var p = document.querySelectorAll("p");
+var question = document.querySelector(".questionText");
 // Initial alert describing game parameters
 alert("Correct answers add 10 seconds to timer. Incorrect answers subtract 10 seconds from timer. High scores are logged so try your best! Press the start button to begin.");
 // Creating functions for button click
@@ -10,12 +10,19 @@ function initialClick() {
     if (document.body.style.background = "url(https://static.makeuseof.com/wp-content/uploads/2017/10/What-Is-JavaScript-Featured-670x335.jpg)") {
         document.body.style.background = "url(https://media.gettyimages.com/videos/4k-rain-multiple-different-layers-isolated-over-black-tested-video-id695625094?s=640x640)"
         btn.innerText = "Check Answer";
+        btn.style.color = "chartreuse";
         btn.style.fontSize = "2.5vw";
-        p.body.fontColor = "chartreuse";
+        btn.style.backgroundColor = "black";
+        btn.style.borderColor = "chartreuse";
     }
 }
+function addText() {
+    question.innerText ="some text"; 
+}
+
 btn.addEventListener("click", function() {
     initialClick();
+    addText();
 });
 
 
