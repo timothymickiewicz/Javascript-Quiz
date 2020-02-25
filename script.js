c580ca4e6644d427e81f923d767d168dac341986
Timothy Mickiewicz <timothy.mickiewicz@gmail.com>
2020-02-25 06:09:43 -0600

    working point counter, rearranged items, created check functions

--- script.js
+++ script.js
@@ -7,36 +7,78 @@ var one = document.getElementById("answer1");
 var two = document.getElementById("answer2");
 var three = document.getElementById("answer3");
 var four = document.getElementById("answer4");
+var hide = document.querySelector(".hide");
+var check = null;
+var correct = 0;
+var incorrect = 0;
+var timer = 60;
 // Initial alert describing game parameters
-alert("Correct answers add 10 seconds to timer. Incorrect answers subtract 10 seconds from timer. High scores are logged so try your best! Press the start button to begin.");
+alert("Welcome to my Javascript Quiz! Correct answers are highlighted in blue and add 10 seconds to the timer. Incorrect answers are highlighted in red and subtract 10 seconds from the timer. You have 60 seconds to complete the quiz. High scores are logged so try your best! Press the start button to begin.");
 // Creating functions for button click
 function initialClick() {
     if (document.body.style.background = "url(https://static.makeuseof.com/wp-content/uploads/2017/10/What-Is-JavaScript-Featured-670x335.jpg)") {
         document.body.style.background = "url(https://media.gettyimages.com/videos/4k-rain-multiple-different-layers-isolated-over-black-tested-video-id695625094?s=640x640)"
         header.style.display = "none";
-        btn.innerText = "Check Answer";
-        btn.style.color = "chartreuse";
-        btn.style.fontSize = "2.5vw";
-        btn.style.backgroundColor = "black";
-        btn.style.borderColor = "chartreuse";
+        btn.style.visibility = "hidden";
+        toggleClass();
+
+// I might use these for a check answer feature later on, currently liking the click button and it does everything on answer selection
+
+        // btn.innerText = "Check Answer";
+        // btn.style.color = "chartreuse";
+        // btn.style.fontSize = "2.5vw";
+        // btn.style.backgroundColor = "black";
+        // btn.style.borderColor = "chartreuse";
     }
 }
 function firstQuestion() {
-    toggleClass();
     question.innerText = "What is the fastest way to loop through an array?"; 
     one.innerText = "for (var i = 0, len = myArray.length; i < len; i++) { }";
     two.innerText = "var len = arr.length; while (len--) { }";
     three.innerText = "for (var i = array.length; i--; )";
     four.innerText = "while( --length ) { //or length--}";
+    one.addEventListener("click", function(){
+        one.style.backgroundColor = "blue";
+        check = true;
+        checkAnswer();
+    })
+    two.addEventListener("click", function(){
+        two.style.backgroundColor = "red";
+        check = false;
+        checkAnswer();
+    })
+    three.addEventListener("click", function(){
+        three.style.backgroundColor = "red";
+        check = false;
+        checkAnswer();
+    })
+    four.addEventListener("click", function(){
+        four.style.backgroundColor = "red";
+        check = false;
+        checkAnswer();
+    })
+}
+// Creating point system
+function checkAnswer() {
+    if (check === true) {
+        correct = correct + 1;
+        timer = timer + 10;
+    }
+    else {
+        check === false;
+        incorrect = incorrect + 1;
+        timer = timer - 10;
+    }
+    console.log(correct);
+    console.log(incorrect);
 }
 // Toggling all classes to quiz mode
 function toggleClass() {
-    for (i=0; i <= 4; i++) {
-        one.classList.toggle("answersJS"),
-        two.classList.toggle("answersJS");
-        three.classList.toggle("answersJS");
-        four.classList.toggle("answersJS");
-    }
+    hide.classList.remove("hide");
+    one.classList.toggle("answersJS"),
+    two.classList.toggle("answersJS");
+    three.classList.toggle("answersJS");
+    four.classList.toggle("answersJS");
 }
 // Button click event
 btn.addEventListener("click", function() {
