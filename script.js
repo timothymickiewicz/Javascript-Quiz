7d17920e99876b735f4fdaeea4858cd17d0ecd38
Timothy Mickiewicz <timothy.mickiewicz@gmail.com>
2020-02-26 06:14:53 -0600

    created high score ids and linked to js

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ var two = document.getElementById("answer2");
 var three = document.getElementById("answer3");
 var four = document.getElementById("answer4");
 var hide = document.querySelector(".hide");
+var hs1 = document.getElementById("highScore1");
+var hs2 = document.getElementById("highScore2");
+var hs3 = document.getElementById("highScore3");
+var hs4 = document.getElementById("highScore4");
+var hs5 = document.getElementById("highScore5");
 var check = null;
 var correct = 0;
 var incorrect = 0;
@@ -16,7 +21,21 @@ var checkClick = null;
 
 // Initial alert describing game parameters
 alert("Welcome to my Javascript Quiz! Correct answers are highlighted in blue and add 10 seconds to the timer. Incorrect answers are highlighted in red and subtract 10 seconds from the timer. You have 60 seconds to complete the quiz. High scores are logged so try your best! Press the start button to begin.");
-
+//Start button click event
+if (correct + incorrect === 0) {
+    btn.addEventListener("click", function() {
+        initialClick();
+        firstQuestion();
+    })
+}
+// Toggling all classes to quiz mode
+function toggleClass() {
+    hide.classList.remove("hide");
+    one.classList.toggle("answersJS"),
+    two.classList.toggle("answersJS");
+    three.classList.toggle("answersJS");
+    four.classList.toggle("answersJS");
+}
 // Creating functions for start button click
 function initialClick() {
     if (document.body.style.background = "url(https://static.makeuseof.com/wp-content/uploads/2017/10/What-Is-JavaScript-Featured-670x335.jpg)") {
@@ -34,103 +53,124 @@ function initialClick() {
         // btn.style.borderColor = "chartreuse";
     
 }
-// functions for question 1
-function nextOneTrue() {
-    trueAnswer();
+// Creating point system
+function checkAnswer() {
+    if (check === true) {
+        correct = correct + 1;
+    }
+    else if (check === false) {
+        incorrect = incorrect + 1;
+        timer = timer - 10;
+    }
+    console.log(correct);
+    console.log(incorrect);
+}
+// Establishing answer as true or false
+function trueAnswer() {
+    check = true;
+    checkAnswer();
+}
+function falseAnswer() {
+    check = false;
+    checkAnswer();
+}
+// Advance to subsequent questions
+function movetoTwo() {
+    if (correct + incorrect === 1) {
+        secondQuestion();
+    }
+}
+function movetoThree() {
+    if (correct + incorrect > 1) {
+        thirdQuestion();
+    }
+}
+// End game page generates after point accumulation of 3
+function gotoEndGame() {
+    if (correct + incorrect > 2) {
+        endGame();
+    }
+}
+// Removing click events after each question
+function removeq1() {
     one.removeEventListener("click", nextOneTrue, true);
     two.removeEventListener("click", nextTwoFalse, true);
     three.removeEventListener("click", nextThreeFalse, true);
     four.removeEventListener("click", nextFourFalse, true);
+}
+function removeq2() {
+    one.removeEventListener("click", q2nextOneFalse, true);
+    two.removeEventListener("click", q2nextTwoFalse, true);
+    three.removeEventListener("click", q2nextThreeTrue, true);
+    four.removeEventListener("click", q2nextFourFalse, true);
+}
+function removeq3() {
+    one.removeEventListener("click", q3nextOneFalse, true);
+    two.removeEventListener("click", q3nextTwoFalse, true);
+    three.removeEventListener("click", q3nextThreeFalse, true);
+    four.removeEventListener("click", q3nextFourTrue, true);
+}
+// functions for question 1
+function nextOneTrue() {
+    trueAnswer();
+    removeq1();
     movetoTwo();
 }
 function nextTwoFalse() {
     falseAnswer();
-    one.removeEventListener("click", nextOneTrue, true);
-    two.removeEventListener("click", nextTwoFalse, true);
-    three.removeEventListener("click", nextThreeFalse, true);
-    four.removeEventListener("click", nextFourFalse, true);
+    removeq1();
     movetoTwo();
 }
 function nextThreeFalse() {
     falseAnswer();
-    one.removeEventListener("click", nextOneTrue, true);
-    two.removeEventListener("click", nextTwoFalse, true);
-    three.removeEventListener("click", nextThreeFalse, true);
-    four.removeEventListener("click", nextFourFalse, true);
+    removeq1();
     movetoTwo();
 }
 function nextFourFalse() {
     falseAnswer();
-    one.removeEventListener("click", nextOneTrue, true);
-    two.removeEventListener("click", nextTwoFalse, true);
-    three.removeEventListener("click", nextThreeFalse, true);
-    four.removeEventListener("click", nextFourFalse, true);
+    removeq1();
     movetoTwo();
 }
 // functions for question 2
 function q2nextOneFalse() {
     falseAnswer();
-    one.removeEventListener("click", q2nextOneFalse, true);
-    two.removeEventListener("click", q2nextTwoFalse, true);
-    three.removeEventListener("click", q2nextThreeTrue, true);
-    four.removeEventListener("click", q2nextFourFalse, true);
+    removeq2();
     movetoThree();
 }
 function q2nextTwoFalse() {
     falseAnswer();
-    one.removeEventListener("click", q2nextOneFalse, true);
-    two.removeEventListener("click", q2nextTwoFalse, true);
-    three.removeEventListener("click", q2nextThreeTrue, true);
-    four.removeEventListener("click", q2nextFourFalse, true);
+    removeq2();
     movetoThree();
 }
 function q2nextThreeTrue() {
     trueAnswer();
-    one.removeEventListener("click", q2nextOneFalse, true);
-    two.removeEventListener("click", q2nextTwoFalse, true);
-    three.removeEventListener("click", q2nextThreeTrue, true);
-    four.removeEventListener("click", q2nextFourFalse, true);
+    removeq2();
     movetoThree();
 }
 function q2nextFourFalse() {
     falseAnswer();
-    one.removeEventListener("click", q2nextOneFalse, true);
-    two.removeEventListener("click", q2nextTwoFalse, true);
-    three.removeEventListener("click", q2nextThreeTrue, true);
-    four.removeEventListener("click", q2nextFourFalse, true);
+    removeq2();
     movetoThree();
 }
 // Functions for question 3
 function q3nextOneFalse() {
     falseAnswer();
-    one.removeEventListener("click", q3nextOneFalse, true);
-    two.removeEventListener("click", q3nextTwoFalse, true);
-    three.removeEventListener("click", q3nextThreeFalse, true);
-    four.removeEventListener("click", q3nextFourTrue, true);
+    removeq3();
     gotoEndGame();
 }
 function q3nextTwoFalse() {
     falseAnswer();
-    one.removeEventListener("click", q3nextOneFalse, true);
-    two.removeEventListener("click", q3nextTwoFalse, true);
-    three.removeEventListener("click", q3nextThreeFalse, true);
-    four.removeEventListener("click", q3nextFourTrue, true);
+    removeq3();
     gotoEndGame();
 }
 function q3nextThreeFalse() {
     falseAnswer();
-    one.removeEventListener("click", q3nextOneFalse, true);
-    two.removeEventListener("click", q3nextTwoFalse, true);
-    three.removeEventListener("click", q3nextThreeFalse, true);
-    four.removeEventListener("click", q3nextFourTrue, true);
+    removeq3();
     gotoEndGame();
 }
 function q3nextFourTrue() {
     trueAnswer();
-    one.removeEventListener("click", q3nextOneFalse, true);
-    two.removeEventListener("click", q3nextTwoFalse, true);
-    three.removeEventListener("click", q3nextThreeFalse, true);
-    four.removeEventListener("click", q3nextFourTrue, true);
+    removeq3();
     gotoEndGame();
 }
 // Creating questions for quiz
@@ -173,64 +213,11 @@ function endGame() {
     two.style.display = "none";
     three.style.display = "none";
     four.style.display = "none";
+    hs1.innerText = correct;
 }
 
-// Creating point system
-function checkAnswer() {
-    if (check === true) {
-        correct = correct + 1;
-    }
-    else if (check === false) {
-        incorrect = incorrect + 1;
-        timer = timer - 10;
-    }
-    console.log(correct);
-    console.log(incorrect);
-}
-
-// Advance to subsequent questions
-function movetoTwo() {
-    if (correct + incorrect === 1) {
-        secondQuestion();
-    }
-}
-function movetoThree() {
-    if (correct + incorrect > 1) {
-        thirdQuestion();
-    }
-}
-
-// End game page generates after point accumulation of 3
-function gotoEndGame() {
-    if (correct + incorrect > 2) {
-        endGame();
-    }
-}
-// Toggling all classes to quiz mode
-function toggleClass() {
-    hide.classList.remove("hide");
-    one.classList.toggle("answersJS"),
-    two.classList.toggle("answersJS");
-    three.classList.toggle("answersJS");
-    four.classList.toggle("answersJS");
-}
-//Start button click event
-if (correct + incorrect === 0) {
-    btn.addEventListener("click", function() {
-        initialClick();
-        firstQuestion();
-    })
-}
-
-function trueAnswer() {
-    check = true;
-    checkAnswer();
-}
-function falseAnswer() {
-    check = false;
-    checkAnswer();
-}
-// make check function, if check function returns value of true, make check answer do an if statement "if check is true, do this and add 10 seconds to timer, else do this and subtract 10 seconds from timer, both lead to next question and log their responses into correct or missed variables, maybe as a number value that gets added to and printed to screen on quiz end page"
-// maybe put all questions into a loop that reiterates until timer hits zero or questions run out
+// create timer that runs during test
+// create high score html that is linked to a hyper link
 console.log(header.textContent);
 console.log(btn.textContent);
+console.log(hs1);
